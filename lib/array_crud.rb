9994ea421a0ffaa79b4cb42a29ae32d5bdd96af9
madelinemc <madeline.coons@gmail.com>
2020-10-02 12:42:42 -0500

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  friends = ["Rachel", "Ross", "Monica", "Chandler"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  new_array = array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  new_array = array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  new_array = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  new_array = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
